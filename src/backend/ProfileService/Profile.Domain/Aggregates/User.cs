be8e85cc30cc730d2765461e9550b7ab0c1d3950
Hforna <henriqueflashpingo@gmail.com>
2025-05-27 18:14:19 -0300

    create initial structure based on ddd

---
+++ src/backend/ProfileService/Profile.Domain/Aggregates/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Profile.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profile.Domain.Aggregates
+{
+    public class User : IdentityUser<long>
+    {
+        public override Email UserEmail 
+        {
+            get => Email 
+        }
+    }
+}
